5cc31ce30aeba8711ff3af68cf13ea305a30cd73
Tyler Niemi <tylerniemi@hotmail.com>
2015-06-27 23:02:21 -0700

    Submit Exercise4

--- lib/class2/exercise4.rb
+++ lib/class2/exercise4.rb
@@ -13,3 +13,14 @@
 #   What's your last name?
 #   Jackson
 #   Nice to meet you, Samuel Leroy Jackson.
+
+puts 'What\'s your first name?'
+firstName = gets.chomp
+
+puts 'What\'s your middle name?'
+middleName = gets.chomp
+
+puts 'What\'s your last name?'
+lastName = gets.chomp
+
+puts 'Nice to meet you, ' + firstName + ' ' + middleName + ' ' + lastName + '.'
