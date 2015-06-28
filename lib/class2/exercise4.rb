61bdbcf7443a826c8f66fbeeab9dffd3e9148e75
Tyler Niemi <tylerniemi@hotmail.com>
2015-06-27 17:43:38 -0700

    Update exercise4.rb

--- lib/class2/exercise4.rb
+++ lib/class2/exercise4.rb
@@ -17,7 +17,7 @@
 puts 'What\'s your first name?'
 firstName = gets.chomp
 
-puts 'What\'s your Middle name?'
+puts 'What\'s your middle name?'
 middleName = gets.chomp
 
 puts 'What\'s your last name?'
