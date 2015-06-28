0296abe31fc53e2ea44ae4d79226d28bca61e9b1
Tyler Niemi <tylerniemi@hotmail.com>
2015-06-27 23:32:29 -0700

    Submit Exercise7.rb

--- lib/class2/exercise7.rb
+++ lib/class2/exercise7.rb
@@ -4,6 +4,12 @@
 #
 #   $ ruby exercise7.rb
 #                   Table of Contents
+# 5 points
+#
+# Write a program that displays the following table of contents:
+#
+#   $ ruby exercise7.rb
+#                   Table of Contents
 #   Chapter 1:  Getting Started                page  1
 #   Chapter 2:  Numbers                        page  9
 #   Chapter 3:  Letters                        page 13
@@ -31,13 +37,4 @@ puts ('Table of Contents'.center(line_width))
 puts ('Chapter 1:  Getting Started'.ljust(10) + 'page  1'.rjust(23))
 puts ('Chapter 2:  Numbers'.ljust(10) + 'page  9'.rjust(31))
 puts ('Chapter 3:  Letters'.ljust(10) + 'page 13'.rjust(31))
-#
-#     Returns a new String of length Integer with `string` right justified.
-#
-#     'bird'.rjust(10)  #=> '      bird'
 
-line_width = 50
-puts ('Table of Contents'.center(line_width))
-puts ('Chapter 1:  Getting Started'.ljust(10) + 'page  1'.rjust(23))
-puts ('Chapter 2:  Numbers'.ljust(10) + 'page  9'.rjust(31))
-puts ('Chapter 3:  Letters'.ljust(10) + 'page 13'.rjust(31))
