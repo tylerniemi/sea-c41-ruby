967f53481f4e61894de8bdfda3f213e621a81df6
Tyler Niemi <tylerniemi@hotmail.com>
2015-06-28 19:16:45 -0700

    Chapter 4 Exercises

--- lib/class2/chapter4.rb
+++ lib/class2/chapter4.rb
@@ -1,1 +1,44 @@
-puts 2 + 5
+puts '...you can say that again...'
+puts '...you can say that again...'
+
+puts ''
+
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+puts ''
+
+name = 'Tyler Jon Elias Niemi'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name
+puts 'is a really long name'
+
+puts ''
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day'
+
+composer = 'Beethoven'
+puts 'But I prefer Beethoven, personally.'
+
+puts ''
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+2)
+puts my_own_var
+
+puts ''
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
