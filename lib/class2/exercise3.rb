8e111877446941f150f4001f9919251e6527fdbd
Tyler Niemi <tylerniemi@hotmail.com>
2015-06-30 15:42:27 -0700

    Submit Exercise3

--- lib/class2/exercise3.rb
+++ lib/class2/exercise3.rb
@@ -3,6 +3,6 @@
 # If a person is 1,160,000,000 seconds old, display their age in years. As
 # always, assume leap years don't exist.
 
-seconds = 1160000000
-year = 31536000
+seconds = 1_160_000_000
+year = 31_536_000
 puts seconds / year
