b40b4f9aa385b70f475019f32ebec101b816ff3a
Tyler Niemi <tylerniemi@hotmail.com>
2015-07-07 13:04:52 -0700

    Submit Exercise1

--- lib/class5/exercise1.rb
+++ lib/class5/exercise1.rb
@@ -32,7 +32,11 @@
 require 'yaml'
 
 def records
-  ['change me']
+  ['Incentivized methodologies',
+   'Seemless innovation',
+   'Corporate synergy',
+   'Scalable globalization',
+   'Monetized assets']
 end
 
 puts records.to_yaml
