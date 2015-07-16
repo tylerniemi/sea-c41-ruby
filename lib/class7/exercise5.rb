642712841714b29ccb3adc680a7c882935df39aa
Tyler Niemi <tylerniemi@hotmail.com>
2015-07-16 11:26:03 -0700

    Submit Exercise5

--- lib/class7/exercise5.rb
+++ lib/class7/exercise5.rb
@@ -37,5 +37,21 @@
 
 # rubocop:disable TrivialAccessors
 class OrangeTree
-  # replace me
+  attr_accessor :fruit
+  def initialize(fruit = 50)
+    @fruit = fruit
+  end
+
+  def pick!(amount = 1)
+    @fruit -= amount if fruit >= amount
+  end
+
+  def ==(other)
+    fruit == other.fruit
+  end
+
+  def pick(amount = 1)
+    return nil if @fruit < amount
+    OrangeTree.new(@fruit - amount)
+  end
 end
